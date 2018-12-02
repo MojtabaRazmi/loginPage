c048ee88c5381a41ea9a8cce31fd7394d1db5f7b
unknown <razmimoj@gmail.com>
2018-12-02 13:01:15 +0330

    2

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {
+    Button, Form, FormGroup, Label, Input
+} from 'reactstrap';
+
+
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            number : "",
+            email : '',
+            password : ""
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                        <div className="card card-signin my-5">
+                            <div className="card-body">
+                                <h2 className="card-title text-center">Sign In</h2>
+                                <Form className="form-signin">
+                                    <FormGroup className="form-label-group">
+                                        <Input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                                            <Label htmlFor="inputEmail">Email address</Label>
+                                    </FormGroup>
+
+                                    <FormGroup className="form-label-group">
+                                        <Input type="number" id="inputPassword" className="form-control" placeholder="number" />
+                                            <Label htmlFor="inputPassword">Phone Number</Label>
+                                    </FormGroup>
+
+                                    <FormGroup className="form-label-group">
+                                        <Input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                                        <Label htmlFor="inputPassword">Password</Label>
+                                    </FormGroup>
+
+                                    <FormGroup className="custom-control custom-checkbox mb-3">
+                                        <Input type="checkbox" className="custom-control-input" id="customCheck1" />
+                                            <Label className="custom-control-label" htmlFor="customCheck1">Remember password</Label>
+                                    </FormGroup>
+                                    <Button outline color='primary' className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in with Email</Button>
+                                    <hr className="my-4" />
+                                        <Button outline color="info" className="btn btn-lg btn-google btn-block text-uppercase" type="submit"> Sign in with Phone Number</Button>
+                                        <Button outline color="danger" className="btn btn-lg btn-facebook btn-block text-uppercase" type="ignore">Cancel</Button>
+                                </Form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Switch , BrowserRouter,Route} from 'react-router-dom'
 import './index.css';
+import './css/login.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LoginPage from './components/LoginPage'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path='/login' component={LoginPage}/>
+            <Route path='/' exact component ={App}/>
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
